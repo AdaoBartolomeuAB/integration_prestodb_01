355c1e08e5207c35fab93bc6f81505dc121faf1d
Adão Gaspar Bartolomeu <adaobartolomeuA.B96@hotmail.com>
2025-07-15 07:46:04 +0100

    first commit

---
+++ src/main/java/ao/kordoge/integration_prestodb/IntegrationPrestodbApplication.java
@@ -0,0 +1,13 @@
+package ao.kordoge.integration_prestodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationPrestodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegrationPrestodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ao/kordoge/integration_prestodb/IntegrationPrestodbApplicationTests.java
@@ -0,0 +1,13 @@
+package ao.kordoge.integration_prestodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationPrestodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
